077c36b2751d47b5d717d18adcbb0d76580bf52d
Adam-Collier <adamcollierart@gmail.com>
2018-01-21 20:10:36 +0000

    created fake data for fetch testing

--- routes/index.js
+++ routes/index.js
@@ -3,22 +3,30 @@ var router = express.Router();
 
 /* GET first API page */
 // Routes are based on the route set for the file
-router.get('/api', function(req, res, next) {
-  res.send([{ 
-    website: 'Airbnb',
-    url: 'https://www.airbnb.co.uk/',
-    img: './client/public/img/airbnb.png',
-    addedBy: 'Adam Collier',
-    date: '17/08/18'
-  },
-  {
-    website: 'ManvsMachine',
-    url: 'https://www.mvsm.co.uk/',
-    img: './client/public/img/mvsm.png',
-    addedBy: 'Adam Collier',
-    date: '19/08/18' 
-  }
-]);
+router.get('/api', function (req, res, next) {
+  res.send([
+    {
+      website: 'ManvsMachine',
+      url: 'https://www.mvsm.co.uk/',
+      img: '/img/mvsm.png',
+      addedBy: 'Adam Collier',
+      date: '19/08/18'
+    },
+    {
+      website: 'Buck Studio',
+      url: 'http://buck.tv/',
+      img: '/img/buck.png',
+      addedBy: 'Adam Collier',
+      date: '21/08/18'
+    },
+    {
+      website: 'Airbnb',
+      url: 'https://www.airbnb.co.uk/',
+      img: '/img/airbnb.png',
+      addedBy: 'Adam Collier',
+      date: '17/08/18'
+    },
+  ]);
 });
 
 module.exports = router;
