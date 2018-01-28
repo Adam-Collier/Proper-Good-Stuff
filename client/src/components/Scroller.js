cbc550adcf85ceee47b2d0c1290fe1337a9d8030
Adam-Collier <adamcollierart@gmail.com>
2018-01-28 15:10:31 +0000

    send request to api with jwt in header

--- client/src/components/Scroller.js
+++ client/src/components/Scroller.js
@@ -1,24 +1,34 @@
 import React, { Component } from 'react';
 
 export default class Scroller extends Component {
-  constructor(){
+  constructor() {
     super()
     this.state = {
       sites: [],
     }
   }
 
-  componentDidMount(){
-    fetch('/api').then(response => {
-      return response.json();
+  componentDidMount() {
+    let localS = localStorage.getItem('token');
+
+    fetch('/api', {
+      headers: new Headers({
+        'jwt': localS
+      })
+    }).then(response => {
+      return response.json()
     }).then(data => {
+      console.log(data);
+      if (data.error) { 
+        return this.props.signout 
+      }
       let sites = data.map(x => {
         console.log(x)
         return (
           <div className="site" key={x.website}>
             <h1>{x.website}</h1>
             <a href={x.url}>{x.url}</a>
-            <img src={x.img} alt=""/>
+            <img src={x.img} alt="" />
             <div>
               <p>{x.date}</p>
               <p>Added by {x.addedBy}</p>
@@ -29,10 +39,9 @@ export default class Scroller extends Component {
       this.setState({ sites: sites })
       console.log("state", this.state.sites)
     })
-    
   }
 
-  render(){
+  render() {
     return (
       <div className="scroller">
         {this.state.sites}
