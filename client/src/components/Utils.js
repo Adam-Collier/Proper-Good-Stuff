4937e1186e3d6ef96442fc1eceb973966765d94a
Adam-Collier <adamcollierart@gmail.com>
2018-03-11 18:55:46 +0000

    onKeyUp passes the value back upto the parent component to set the state

--- client/src/components/Utils.js
+++ client/src/components/Utils.js
@@ -1,44 +1,87 @@
-import React from 'react';
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
 
-const Search = (props) => {
-  return(
+const Search = props => {
+  console.log(props);
+  return (
     <div className="search">
-      <input type="text" />
-      <svg width="100%" height="100%" viewBox="0 0 27 26" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" xmlSpace="preserve" style={{ fillRule: 'evenodd', clipRule: 'evenodd', strokeLinecap: 'round', strokeLinejoin: 'round', strokeMiterlimit: '1.41421' }}><circle cx="10.136" cy="10.136" r="7.56" style={{ fill: 'none', strokeWidth: '2.97px', stroke: '#000' }} /><path d="M15.884,15.014l7.869,7.87" style={{ fill: 'none', strokeWidth: '2.97px', stroke: '#000' }} /></svg>
+      <input
+        type="text"
+        onKeyUp={event => props.onTextChange(event.target.value)}
+      />
+      <svg
+        width="100%"
+        height="100%"
+        viewBox="0 0 27 26"
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+        xmlSpace="preserve"
+        style={{
+          fillRule: "evenodd",
+          clipRule: "evenodd",
+          strokeLinecap: "round",
+          strokeLinejoin: "round",
+          strokeMiterlimit: "1.41421"
+        }}
+      >
+        <circle
+          cx="10.136"
+          cy="10.136"
+          r="7.56"
+          style={{ fill: "none", strokeWidth: "2.97px", stroke: "#000" }}
+        />
+        <path
+          d="M15.884,15.014l7.869,7.87"
+          style={{ fill: "none", strokeWidth: "2.97px", stroke: "#000" }}
+        />
+      </svg>
     </div>
-  )
+  );
+};
+
+class Tabs extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      color: "black"
+    };
+  }
+
+  handleClick(e, isSite) {
+    this.setState({
+      color: "grey"
+    });
+    console.log(isSite);
+    this.props.switchView(isSite);
+    console.log(this.props);
+    document.querySelectorAll("h1").forEach(x => {
+      x.classList.remove("active");
+    });
+    e.target.classList.add("active");
+  }
+
+  render() {
+    return (
+      <div className="nav">
+        <h1 onClick={e => this.handleClick(e, true)} className="active">
+          Your Sites
+        </h1>
+        <h1 onClick={e => this.handleClick(e, false)}>Account</h1>
+      </div>
+    );
+  }
 }
 
-// const buttonStyles = {
-//   padding: '3px 20px',
-//   background: 'none',
-//   borderRadius: '3px',
-//   marginLeft: '15px',
-//   fontSize: '14px',
-//   cursor: 'pointer',
-// }
-
-// const Button = (props) => {
-//   return (
-//     <button style={buttonStyles}>{props.name}</button>
-//   )
-// }
-
-// const Filter = () => {
-//   return(
-//     <div>
-//       <Button name="newest" />
-//       <Button name="oldest" />
-//       <Button name="author" />
-//     </div>
-//   )
-// }
-
-export const Utils = () => {
-  return(
+const Utils = props => {
+  return (
     <div className="utils">
-      <Search />
-      {/* <Filter /> */}
+      <Search onTextChange={props.onTextChange} />
+      {props.location.pathname === "/profile" ? (
+        <Tabs switchView={props.switchView} />
+      ) : null}
     </div>
-  )
-}
+  );
+};
+
+export default withRouter(Utils);
