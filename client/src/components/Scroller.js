8a592f528555975aa629fec4d6aad36854a443f0
Adam-Collier <adamcollierart@gmail.com>
2018-03-11 18:21:31 +0000

    data fetching moved into parent component & now shows latest added

--- client/src/components/Scroller.js
+++ client/src/components/Scroller.js
@@ -1,54 +1,29 @@
 import React, { Component } from 'react';
 
 export default class Scroller extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      sites: [],
-    }
-  }
 
-  componentDidMount(props) {
-    let localS = localStorage.getItem('token');
-
-    fetch('/api', {
-      headers: new Headers({
-        'jwt': localS
-      })
-    }).then(response => {
-      return response.json()
-    }).then(data => {
-      console.log(data);
-      if (data.error === 'jwt expired') { 
-        console.log(this.props.signout)
-        return(
-          this.props.signout()
-        )
-      }else{
-        let sites = data.map(x => {
-          console.log(x)
-          return (
-            <div className="site" key={x.website}>
-              <h1>{x.website}</h1>
-              <a href={x.url}>{x.url}</a>
-              <img src={x.img} alt="" />
-              <div>
-                <p>{x.date}</p>
-                <p>Added by {x.addedBy}</p>
-              </div>
+  getSites() {
+    console.log(this.props.fData);
+      return this.props.fData.map((x, i) => {
+        console.log(x)
+        return (
+          <div className="site" key={i}>
+            <h2>{x.website}</h2>
+            <a href={x.url}>{x.url}</a>
+            <img src={x.img} alt="" />
+            <div>
+              <p>{x.date}</p>
+              <p>Added by {x.addedBy}</p>
             </div>
-          )
-        })
-        this.setState({ sites: sites })
-        console.log("state", this.state.sites)
-      }
-    })
-  }
+          </div>
+        )
+      }).reverse()
+    }
 
   render() {
     return (
       <div className="scroller">
-        {this.state.sites}
+        {this.getSites()}
       </div>
     )
   }
