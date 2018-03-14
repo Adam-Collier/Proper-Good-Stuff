2ad8daa43eb034710109d773c5a2378127487996
Adam-Collier <adamcollierart@gmail.com>
2018-03-14 08:56:58 +0000

    form values removed after submit

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class Form extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      siteTitle: '',
-      siteUrl: ''
+      siteTitle: "",
+      siteUrl: ""
     };
 
     this.handleInputChange = this.handleInputChange.bind(this);
@@ -23,40 +23,52 @@ export default class Form extends Component {
   }
 
   handleSubmit(event) {
-
     this.props.loader();
 
-    console.log(this.state.siteTitle)
-    let localS = localStorage.getItem('token');
+    console.log(this.state.siteTitle);
+    let localS = localStorage.getItem("token");
     // alert('A name was submitted: ' + this.state.value);
     event.preventDefault();
-    console.log(this.props.fData())
-    fetch('/api', {
-      method: 'POST',
+    console.log(this.props.fData());
+    fetch("/api", {
+      method: "POST",
       body: JSON.stringify({
         website: this.state.siteUrl,
         title: this.state.siteTitle
       }),
       headers: new Headers({
-        'Content-Type': 'application/json',
-        'jwt': localS
+        "Content-Type": "application/json",
+        jwt: localS
       })
     }).then(() => {
-      console.log('posted');
+      console.log("posted");
       this.props.fData();
       setTimeout(() => {
         this.props.loader();
       }, 300);
     });
+    this.setState({ siteTitle: "", siteUrl: "" });
   }
 
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
-        <input type="text" placeholder="title" name= "siteTitle" value={this.state.siteTitle} onChange={this.handleInputChange} />
-        <input type="text" placeholder="url" name= "siteUrl" value={this.state.siteUrl} onChange={this.handleInputChange}/>
+        <input
+          type="text"
+          placeholder="title"
+          name="siteTitle"
+          value={this.state.siteTitle}
+          onChange={this.handleInputChange}
+        />
+        <input
+          type="text"
+          placeholder="url"
+          name="siteUrl"
+          value={this.state.siteUrl}
+          onChange={this.handleInputChange}
+        />
         <input type="submit" value="Add site" />
       </form>
-    )
+    );
   }
 }
