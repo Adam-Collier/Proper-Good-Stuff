84851f81d8f859b23706ca19c5526b931e4df88f
Adam-Collier <adamcollierart@gmail.com>
2018-03-31 20:33:02 +0100

    removed service worker

--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import {
-  BrowserRouter as Router
-} from 'react-router-dom'
-
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
 
 let Root = () => {
-  return(
+  return (
     <Router>
       <App />
     </Router>
-  )
-}
+  );
+};
 
-ReactDOM.render(<Root />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<Root />, document.getElementById("root"));
