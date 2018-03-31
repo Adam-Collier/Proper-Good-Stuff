f6612e69b024ff4bc40775af75628ee49293d2db
Adam-Collier <adamcollierart@gmail.com>
2018-03-31 20:46:44 +0100

    fixed route issue

--- app.js
+++ app.js
@@ -41,12 +41,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "/client/build")));
 
-// app.use("/", index);
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "client/build", "index.html"));
-});
+app.use("/", index);
 app.use("/users", users);
 app.use("/delete", index);
 app.use("/login", index);
