d0e5bc5aaf791b3235f5ee00e82927ec4b5eafbb
Adam-Collier <adamcollierart@gmail.com>
2018-04-02 15:41:34 +0100

    generate scroller and opening text

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React from "react";
 
 export const Home = () => {
+  let arr = [
+    "https://res.cloudinary.com/dxpmsnd8y/image/upload/v1522591908/t4ksf9hrg47q4n9burui.jpg",
+    "https://res.cloudinary.com/dxpmsnd8y/image/upload/v1519804784/tupnbhxia5mv3lcm6dim.jpg",
+    "https://res.cloudinary.com/dxpmsnd8y/image/upload/v1520429303/nxo4bbvwtzxzhxvytvuf.jpg",
+    "https://res.cloudinary.com/dxpmsnd8y/image/upload/v1522591908/t4ksf9hrg47q4n9burui.jpg",
+    "https://res.cloudinary.com/dxpmsnd8y/image/upload/v1519804784/tupnbhxia5mv3lcm6dim.jpg",
+    "https://res.cloudinary.com/dxpmsnd8y/image/upload/v1520429303/nxo4bbvwtzxzhxvytvuf.jpg"
+  ];
+
+  let scrollSites = () => {
+    return arr.map((x, i) => {
+      console.log(x);
+      return (
+        <div key={i}>
+          <h2>blah blah</h2>
+          <a>https://github.com</a>
+          <img src={x} alt="" />
+          <div>
+            <p>blah blah</p>
+            <p>Added by Steve</p>
+          </div>
+        </div>
+      );
+    });
+  };
+
   return (
-    <div>
-      <h1>
-        This is the original homepage  
-      </h1>
+    <div className="home">
+      <div>
+        <h1>keep a visual reference </h1>
+        <h1>of the sites you and your team love</h1>
+      </div>
+      <div className="scrollSites">{scrollSites()}</div>
     </div>
-  )
-}
+  );
+};
