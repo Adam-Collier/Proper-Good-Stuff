460866e6b8fe8fa802108f5578cb032690d1b4a5
Adam-Collier <adamcollierart@gmail.com>
2018-04-04 23:08:21 +0100

    mobile property added

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
-var crypto = require('crypto');
-var bcrypt = require('bcrypt-nodejs');
-var mongoose = require('mongoose');
+var crypto = require("crypto");
+var bcrypt = require("bcrypt-nodejs");
+var mongoose = require("mongoose");
 
 var schemaOptions = {
   timestamps: true,
@@ -9,45 +9,53 @@ var schemaOptions = {
   }
 };
 
-var userSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  team: String,
-  slackId: String,
-  sites: [{
-    website: String,
-    url: String,
-    img: String,
-    addedBy: String,
-    date: String
-  }]
-}, schemaOptions);
+var userSchema = new mongoose.Schema(
+  {
+    name: String,
+    email: String,
+    team: String,
+    slackId: String,
+    sites: [
+      {
+        website: String,
+        url: String,
+        desktop: String,
+        addedBy: String,
+        date: String,
+        mobile: String
+      }
+    ]
+  },
+  schemaOptions
+);
 
-userSchema.pre('save', function (next) {
+userSchema.pre("save", function(next) {
   var user = this;
-  if (!user.isModified('password')) { return next(); }
-  bcrypt.genSalt(10, function (err, salt) {
-    bcrypt.hash(user.password, salt, null, function (err, hash) {
+  if (!user.isModified("password")) {
+    return next();
+  }
+  bcrypt.genSalt(10, function(err, salt) {
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
       user.password = hash;
       next();
     });
   });
 });
 
-userSchema.methods.comparePassword = function (password, cb) {
-  bcrypt.compare(password, this.password, function (err, isMatch) {
+userSchema.methods.comparePassword = function(password, cb) {
+  bcrypt.compare(password, this.password, function(err, isMatch) {
     cb(err, isMatch);
   });
 };
 
 userSchema.options.toJSON = {
-  transform: function (doc, ret, options) {
+  transform: function(doc, ret, options) {
     delete ret.password;
     delete ret.passwordResetToken;
     delete ret.passwordResetExpires;
   }
 };
 
-var User = mongoose.model('User', userSchema);
+var User = mongoose.model("User", userSchema);
 
 module.exports = User;
