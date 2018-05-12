a605ca2a2fe5259d342aea2801bb6b3f363ba8ca
Adam-Collier <adamcollierart@gmail.com>
2018-05-12 15:47:05 +0100

    render Loader component

--- client/src/components/Scroller.js
+++ client/src/components/Scroller.js
@@ -1,28 +1,54 @@
 import React, { Component } from "react";
+import Loader from "./Loader";
 
 export default class Scroller extends Component {
   getSites() {
     // console.log(this.props.fData);
+
     console.log(this.props.deviceSwitch);
+
+    let sitesLength = this.props.fData.length - this.props.loaderInfo.length;
+
+    if (
+      this.props.loaderInfo.length == 0 ||
+      !this.props.loaderInfo[this.props.loaderInfo.length - 1].desktop
+    ) {
+      sitesLength = this.props.fData.length;
+    }
+
     return this.props.fData
       .map((x, i) => {
-        console.log(x);
-        return (
-          <div className="site" key={i}>
-            <h2>{x.website}</h2>
-            <a href={x.url}>{x.url}</a>
-            <img src={x[this.props.deviceSwitch]} alt="" />
-            <div>
-              <p>{x.date}</p>
-              <p>Added by {x.addedBy}</p>
+        if (i >= sitesLength) {
+          console.log("this is being hit");
+          return;
+        } else {
+          return (
+            <div className="site" key={i}>
+              <h2>{x.website}</h2>
+              <a href={x.url}>{x.url}</a>
+              <img src={x[this.props.deviceSwitch]} alt="" />
+              <div>
+                <p>{x.date}</p>
+                <p>Added by {x.addedBy}</p>
+              </div>
             </div>
-          </div>
-        );
+          );
+        }
       })
       .reverse();
   }
 
   render() {
-    return <div className="scroller">{this.getSites()}</div>;
+    return (
+      <div className="scroller">
+        {/* {this.props.loader ? this.createLoader(this.props.loaderInfo) : null} */}
+        <Loader
+          loader={this.props.loader}
+          loaderInfo={this.props.loaderInfo}
+          device={this.props.deviceSwitch}
+        />
+        {this.getSites()}
+      </div>
+    );
   }
 }
